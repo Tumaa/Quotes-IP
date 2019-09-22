d8db54492cb5353b2ef31b9e582c4960734a472f
tumaa <ramanfatuu@gmail.com>
2019-09-22 13:29:09 +0300

    create directive to strikethrough

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule
