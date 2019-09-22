7c6ebc834e3a8b81969263770123dc0529e71eaa
tumaa <ramanfatuu@gmail.com>
2019-09-22 12:58:52 +0300

    create constructor and add showDescription:boolean to be false

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    id:number ;
-    name: string;
-    author:string;
-    quote:string;
+    showDescription: boolean;
+    constructor(public id:number, public name: string,public author:string, public quote: string){
+   this.showDescription=false;
+}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,14 +7,14 @@ import {Quote} from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes=[
-    {id:1, name:'tumaa', author:'ali', quote:'never give up'},
-    {id:2, name:'tumaa', author:'ali', quote:'never give up'},
-    {id:3, name:'tumaa', author:'ali', quote:'never give up'},
-    {id:4, name:'tumaa', author:'ali', quote:'never give up'},
-    {id:5, name:'tumaa', author:'ali', quote:'never give up'},
-    {id:6, name:'tumaa', author:'ali', quote:'never give up'},
-    
+  quotes: Quote[] = [
+    new Quote  ( 1,  'tumaa',  'ali',  'never give up' ),
+    new Quote  ( 2,  'tumaa',  'ali',  'never give up' ),
+    new Quote (3, 'tumaa',  'ali', 'never give up' ),
+    new Quote  ( 4, 'tumaa', 'ali', 'never give up'), 
+    new Quote  ( 5, 'tumaa', 'ali',  'never give up' ),
+    new Quote ( 6, 'tumaa',  'ali', 'never give up' ),
+
   ];
   constructor() { }
 
